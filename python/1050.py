f415cedac130d354ff612b510f5866b32f18c03d
rianmartins07 <rianmartins@live.com>
2023-07-13 23:17:12 -0300

    1051

---
+++ python/1050.py
@@ -0,0 +1,22 @@
+e = int(input())
+
+
+if e == 61:
+    print("Brasilia");
+elif e == 71:
+    print("Salvador");
+elif e == 11:
+    print("Sao Paulo");
+elif e == 21:
+    print("Rio de Janeiro");
+elif e == 32:
+    print("Juiz de Fora");
+elif e == 19:
+    print("Campinas");
+elif e == 27:
+    print("Vitoria");
+elif e == 31:
+    print("Belo Horizonte");
+else:
+    print("DDD nao cadastrado");
+
