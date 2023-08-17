cb2b7723dbfe5cb4e627e21467b962d8a54d389e
rianmartins <rianmartins@live.com>
2023-08-17 09:45:44 -0300

    1158 fix

--- js/1158.js
+++ js/1158.js
@@ -4,11 +4,11 @@ var lines = input.split('\n')
 n = lines.shift()
 
 for(i=0;i<n;i++){
-    e = lines.shift()
-    console.log(e)
-    x = parseInt(e[0])
-    y = parseInt(e[1])
+    e = lines.shift().split(' ').map(x => parseInt(x))
 
+
+    x = e[0]
+    y = e[1]
     cont=0
     for (j=0;j<y*2;j++){
         if (x%2!=0){
