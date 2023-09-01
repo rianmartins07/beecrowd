761821ab2df7c37f3d26448e1cdea1037c8f68cf
rianmartins07 <rianmartins@live.com>
2023-09-01 14:26:12 -0300

    1188

---
+++ php/1188.php
@@ -0,0 +1,25 @@
+<?php
+$s = trim(fgets(STDIN));
+$m = array();
+for ($i=0;$i<12;$i++){
+    for ($j=0;$j<12;$j++){
+        $m[$i][$j] = floatval(fgets(STDIN));
+    }
+}
+$soma = 0;
+for ($i=0;$i<12;$i++){
+    for ($j=0;$j<12;$j++){
+        if ($j+$i >=12 && $j<$i && $i!=$j){
+            $soma+=$m[$i][$j];
+        }
+    }
+}
+
+if ($s=='S'){
+    printf("%.1lf\n", $soma);
+}else if ($s=='M'){
+    printf("%.1lf\n", $soma/30);
+}
+
+
+?>
