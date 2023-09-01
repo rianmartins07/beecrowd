4ca5e04cb0b1eec04b479649935d39b6ea4df050
rianmartins07 <rianmartins@live.com>
2023-09-01 09:15:21 -0300

    1184

---
+++ js/1184.js
@@ -0,0 +1,26 @@
+var input = require('fs').readFileSync('./dev/stdin', 'utf8')
+var lines = input.split('\n')
+s = lines.shift()
+m = []
+for (i=0;i<12;i++){
+    linha = []
+    for (j=0;j<12;j++){
+        linha.push(parseFloat(lines.shift()))
+    }
+    m.push(linha)
+}
+
+let soma = 0
+for (i=0;i<12;i++){
+    for (j=0;j<12;j++){
+        if (j<i){
+            soma+=m[i][j]
+        }
+    }
+}
+
+if (s=='S'){
+    console.log(soma.toFixed(1))
+}else if (s=='M'){
+    console.log((soma/66).toFixed(1))
+}
