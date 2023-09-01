d145b313be88c94012c400cca39c929d88b1d62b
rianmartins07 <rianmartins@live.com>
2023-09-01 09:19:28 -0300

    1186

---
+++ python/1186.py
@@ -0,0 +1,18 @@
+s = str(input())
+matriz = []
+for i in range(0,12):
+    linha = []
+    for j in range(0,12):
+        linha.append(float(input()))
+    matriz.append(linha)
+soma = 0
+
+for i in range(0,12):
+    for j in range(0,12):
+        if j+i>=12:
+            soma+=matriz[i][j]
+if s=='S':
+    print(f"{soma:.1f}")
+elif s=='M':
+    soma/=66
+    print(f"{soma:.1f}")
